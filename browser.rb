63adef85489489cfc3cf500984da5d2484bb1f47
DRAJ <davidrajnus@gmail.com>
2017-04-11 08:50:59 +0800

    completed

--- browser.rb
+++ browser.rb
@@ -1,15 +1,24 @@
 require 'net/http'
 require 'nokogiri'
+require 'open-uri'
 
-require_relative 'util'
+require_relative 'page'
 
 class Browser
   def run!
     # Run the browser
     # Display a prompt for a user
+    puts "url>"
     # Parse their input
+    url = gets.chomp
+    puts "Fetching..."
     # Display useful results by instantiating
     #   a new Page and calling methods on it.
+    page = Page.new(url)
+    page.fetch!
+    puts "Title: #{page.title}"
+    puts "Content length: #{page.calculate_char_count} characters"
+    page.links
     
     # Questions:
     #  1. How can a user quit the browser gracefully?
--- page.rb
+++ page.rb
@@ -1,14 +1,31 @@
+require 'net/http'
+require 'nokogiri'
+require 'open-uri'
+
+
 class Page
   def initialize(url)
+    @url = url
   end
   
   def fetch!
+    @doc = Nokogiri::HTML(open(@url))
   end
   
   def title
+    @doc.title
   end
   
   def links
+    @links = []
+    links_before_filter = @doc.search('a').map{ |link| link['href'] }
+    links_before_filter.map! {|link| link.to_s}
+    links_before_filter.each do |link|
+      if link.start_with?"http://"
+        @links << link
+      end
+    end
+    @links
     # Research alert!
     # How do you use Nokogiri to extract all the link URLs on a page?
     #
@@ -16,4 +33,10 @@ class Page
     #   <a href="http://somesite.com/page.html">Click here!</a>
     # This would pull out "http://somesite.com/page.html"
   end
+
+  def calculate_char_count
+      @paragraphs = @doc.css("div.zn-body__paragraph")
+      p @paragraphs.inner_text.length
+    end
+
 end
